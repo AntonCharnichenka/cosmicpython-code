94367609008c87982cadfc0c47ba266ef73cce17
Anton Chernichenko <anton.chernichenko@dohop.com>
2023-02-05 11:13:38 +0100

    migrate to sqllachemy 2.0.1

--- tests/integration/test_repository.py
+++ tests/integration/test_repository.py
@@ -1,4 +1,5 @@
 # pylint: disable=protected-access
+from sqlalchemy.sql import text
 from allocation.domain import model
 from allocation.adapters import repository
 
@@ -11,41 +12,47 @@ def test_repository_can_save_a_batch(session):
     session.commit()
 
     rows = session.execute(
-        'SELECT reference, sku, _purchased_quantity, eta FROM "batches"'
+        text('SELECT reference, sku, _purchased_quantity, eta FROM "batches"')
     )
     assert list(rows) == [("batch1", "RUSTY-SOAPDISH", 100, None)]
 
 
 def insert_order_line(session):
     session.execute(
-        "INSERT INTO order_lines (orderid, sku, qty)"
-        ' VALUES ("order1", "GENERIC-SOFA", 12)'
+        text(
+            "INSERT INTO order_lines (orderid, sku, qty)"
+            ' VALUES ("order1", "GENERIC-SOFA", 12)'
+        )
     )
     [[orderline_id]] = session.execute(
-        "SELECT id FROM order_lines WHERE orderid=:orderid AND sku=:sku",
-        dict(orderid="order1", sku="GENERIC-SOFA"),
+        text(
+            "SELECT id FROM order_lines WHERE orderid=:orderid AND sku=:sku"
+        ).bindparams(orderid="order1", sku="GENERIC-SOFA"),
     )
     return orderline_id
 
 
 def insert_batch(session, batch_id):
     session.execute(
-        "INSERT INTO batches (reference, sku, _purchased_quantity, eta)"
-        ' VALUES (:batch_id, "GENERIC-SOFA", 100, null)',
-        dict(batch_id=batch_id),
+        text(
+            "INSERT INTO batches (reference, sku, _purchased_quantity, eta)"
+            ' VALUES (:batch_id, "GENERIC-SOFA", 100, null)'
+        ).bindparams(batch_id=batch_id)
     )
     [[batch_id]] = session.execute(
-        'SELECT id FROM batches WHERE reference=:batch_id AND sku="GENERIC-SOFA"',
-        dict(batch_id=batch_id),
+        text(
+            'SELECT id FROM batches WHERE reference=:batch_id AND sku="GENERIC-SOFA"',
+        ).bindparams(batch_id=batch_id)
     )
     return batch_id
 
 
 def insert_allocation(session, orderline_id, batch_id):
     session.execute(
-        "INSERT INTO allocations (orderline_id, batch_id)"
-        " VALUES (:orderline_id, :batch_id)",
-        dict(orderline_id=orderline_id, batch_id=batch_id),
+        text(
+            "INSERT INTO allocations (orderline_id, batch_id)"
+            " VALUES (:orderline_id, :batch_id)",
+        ).bindparams(orderline_id=orderline_id, batch_id=batch_id)
     )
 
 
